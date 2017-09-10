6ea8ff812cf513e5cc5901e4b14b694f741bea05
Abror Siddikov <abror.siddikov@gatehelpers.com>
2017-09-10 23:20:42 +0200

    #

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app=express();
+const PORT = process.env.PORT || 9000;
 app.use('/lib', express.static('lib'));
 app.use('/app', express.static('app'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/app/index.html');
 });
 
-app.listen(9000, function(){
-  console.log('listening on *:9000');
+app.listen(PORT, function(err){
+	if(err){
+		console.error(err);
+	}else{
+		console.log('listening on *:'+PORT);
+	}
+  
 });
 
